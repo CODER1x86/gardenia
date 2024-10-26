05c979df085da3e0ee930594b71a375c388a1a10
Glitch (truth-aquatic-kip) <none>
2024-10-26 17:08:29 +0000

    🎣🍐 Checkpoint ./public/main.js:71348705/18

--- public/main.js
+++ public/main.js
@@ -24,12 +24,12 @@ function fetchData() {
 }
 
 function loadHeaderFooter() {
-  fetch("header.html")
+  fetch("/public/header.html")
     .then((response) => response.text())
     .then((html) => {
       document.getElementById("header-placeholder").innerHTML = html;
     });
-  fetch("footer.html")
+  fetch("/public/footer.html")
     .then((response) => response.text())
     .then((html) => {
       document.getElementById("footer-placeholder").innerHTML = html;
