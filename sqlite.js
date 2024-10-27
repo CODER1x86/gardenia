33c385e70851fd9fe3576f8491fa77e36ce4abc1
Glitch (truth-aquatic-kip) <none>
2024-10-27 01:12:05 +0000

    🎿🍿 Checkpoint ./public/tos.html:71348705/6891 ./sqlite.js1old:71348705/3048 ./sqlite.js:71348705/1542

---
+++ sqlite.js
@@ -0,0 +1,41 @@
+const fs = require("fs");
+const path = require("path");
+const sqlite3 = require("sqlite3").verbose();
+const dbWrapper = require("sqlite");
+
+const dbFile = path.join(__dirname, "./.data/database.db");
+let db;
+
+// Open database without table creation
+dbWrapper.open({ filename: dbFile, driver: sqlite3.Database }).then(dBase => {
+  db = dBase;
+  console.log("Database initialized successfully");
+}).catch(dbError => {
+  console.error("Error initializing database:", dbError);
+});
+
+module.exports = { 
+  getExpenses: async () => {
+    try {
+      return await db.all("SELECT * FROM expenses");
+    } catch (dbError) {
+      console.error("Error fetching expenses:", dbError);
+    }
+  },
+  addExpense: async expense => {
+    let success = false;
+    try {
+      success = await db.run("INSERT INTO expenses (category, item, price, expense_date, last_updated) VALUES (?, ?, ?, ?, ?)", [
+        expense.category,
+        expense.item,
+        expense.price,
+        expense.expense_date,
+        expense.last_updated
+      ]);
+    } catch (dbError) {
+      console.error("Error adding expense:", dbError);
+    }
+    return success.changes > 0 ? true : false;
+  },
+  // Add more methods for other operations as needed
+};
